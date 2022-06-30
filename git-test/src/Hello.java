085f14191e5062245994a562a8505619e9fcab0e
TanLei <3489094864@qq.com>
2022-06-30 08:59:32 +0800

    first commit

---
+++ git-test/src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Creat by TanLei
+ * Description:
+ * User:86183
+ * Date:2022-06-16
+ * Time:13:51
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
